ba2c85285c430f0f01db09ff9e0d9b9ab1a47823
Jean-Louis Danielo <jeanlouis.danielo@sfr.fr>
2016-02-22 10:58:07 +0100

    Add dev files

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+var nunjucksRender = require('gulp-nunjucks-render');
+
+
+gulp.task('templates', function(req, res) {
+  nunjucksRender.nunjucks.configure(['./dev/views/'], {watch: true});
+    return gulp.src('./dev/views/*.html')
+      .pipe(nunjucksRender())
+      .pipe(gulp.dest('public'));
+});
+
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+  gulp.src('./dev/assets/stylesheets/**/*.scss')
+    .pipe(sass({outputStyle: 'outputStyle'}).on('error', sass.logError))
+    .pipe(gulp.dest('./public/assets/styles'));
+});
+
+gulp.task('images', function () {
+  gulp.src('./dev/assets/images/**/*')
+    .pipe(gulp.dest('./public/assets/images/'));
+});
+
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var babelify = require("babelify");
+
+
+gulp.task('browserify', function() {
+    return browserify('./dev/assets/scripts/app.js').transform("reactify")
+        .bundle()
+        //Pass desired output filename to vinyl-source-stream
+        .pipe(source('app.js'))
+        // Start piping stream to tasks!
+        .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('default', ['templates', 'browserify', 'sass', 'images']);
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch('./dev/assets/stylesheets/**/*.scss', ['sass']);
+    gulp.watch('./dev/views/**/*.html', ['templates']);
+    gulp.watch('./dev/assets/scripts/**/*.js', ['browserify']);
+    gulp.watch('./dev/assets/images/**/*', ['images']);
+});
