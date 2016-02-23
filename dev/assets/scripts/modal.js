4fc640e1ea7834280962a447e6f5012ea5dbcfe8
djeanlouis <danielo.jean-louis@digitaslbi.fr>
2016-02-23 16:19:10 +0100

    add moves to modal

--- dev/assets/scripts/modal.js
+++ dev/assets/scripts/modal.js
@@ -16,6 +16,7 @@ var ModalHeader = React.createClass({
             <img className="pkmn-img" height="40" src={this.props.datas.sprite}/> 
             <span className="pkmn-name">{this.props.datas.name}</span>
           </h4>
+          <Types datas={this.props.datas.types} />
       </div>
     )
   }
@@ -25,7 +26,9 @@ var ModalBody = React.createClass({
   render: function() {
     return (
       <div className="modal-body">
-        <Evolutions datas={this.props.datas} />
+        <Evolutions datas={this.props.datas.evolutions} />
+        <Abilities datas={this.props.datas.abilities} />
+        <MovesContainer datas={this.props.datas.moves} />
       </div>
     )
   }
@@ -46,14 +49,129 @@ var PokemonEvolution = React.createClass({
       </li>
     )
   }
-})
+});
+
+
+var Types = React.createClass({
+  render: function () {
+    if (!this.props.datas) {
+      return null;
+    };
+
+    var types = this.props.datas.map(function(type, index){
+      return <li className={classNames(type.name)} key={index}>{type.name}</li>
+    });
+
+    return(
+      <ul className="list-unstyled pkmn-types">
+        {types}
+      </ul>
+    )
+  }
+});
+
+
+var MovesHeader = React.createClass({
+  render: function() {
+    return (
+      <div className="panel-heading" role="tab" id={'heading' + this.props.id}>
+        <h4 className="panel-title">
+          <a role="button" data-toggle="collapse" data-parent="#accordion" href={'#collapse' + this.props.id}>{this.props.datas}</a>
+        </h4>
+      </div>
+    )
+  }
+});
+
+
+var MovesListItem = React.createClass({
+  render: function() {
+
+    return (
+      <tr>
+        <td>{this.props.datas.name}</td>
+        {this.props.datas.level ? <td>{this.props.datas.level} </td> : null }
+      </tr>
+    )
+  }
+
+});
+
+// List every move for a given category
+var MovesList = React.createClass({
+  render: function() {
+    var move = [];
+    var moves = this.props.datas.map(function(movePkmn) {
+      return <MovesListItem datas={movePkmn} key={movePkmn.name} />;
+    });
+
+    return (
+      <div className="panel-collapse collapse" role="tabpanel" id={'collapse' + this.props.id}>
+        <table className="table">
+          <tbody>
+            { moves }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+});
+
+
+var MovesContainer = React.createClass({
+  render: function () {
+    if (!this.props.datas || _.isEmpty(this.props.datas)) {
+      return null;
+    };
+
+    var tableMoves = [];
+    // We sort alphabetically the type of the attacks
+    var moves = Object.keys(this.props.datas).sort().forEach(function(typeMoves, index, array) {
+      tableMoves.push(<MovesHeader datas={typeMoves} key={typeMoves} id={index} />)
+      tableMoves.push(<MovesList datas={this.props.datas[typeMoves]} key={index}  id={index}/>)
+    }.bind(this));
+
+    return(
+      <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+        <h4 className="caracteristic">Moves list</h4>
+        <div className="panel panel-default">
+          {tableMoves}
+        </div>
+      </div>
+    )
+  }
+});
+
+
+var Abilities = React.createClass({
+  render: function () {
+    if (!this.props.datas || this.props.datas.length === 0) {
+      return null;
+    };
+
+    var abilities = this.props.datas.map(function(ability, index) {
+      return <li className="pkmn-abilities__item" key={index}>{ability}</li>;
+    });
+
+    return (
+      <div>
+        <h4 className="caracteristic">Abilities</h4>
+        <ul className="pkmn-abilities list-unstyled">
+          { abilities }
+        </ul>
+      </div>
+    )
+  }
+});
+
 
 var Evolutions = React.createClass({
   render: function() {
-    if (!this.props.datas.evolutions) {
+    if (!this.props.datas || this.props.datas.length === 0) {
       return null;
     };
-    var evolutions = this.props.datas.evolutions.map(function(evolution, index) {
+
+    var evolutions = this.props.datas.map(function(evolution, index) {
       return <PokemonEvolution datas={evolution} key={index}/>;
     });
 
@@ -78,11 +196,6 @@ var Modal = React.createClass({displayName: 'Modal',
           <div className="modal-content">
             <ModalHeader datas={this.props.pokemon} />
             <ModalBody datas={this.props.pokemon} />
-
-            <div className="modal-footer">
-              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
-            </div>
           </div>
         </div>
       </div>
