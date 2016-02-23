2e4325c7e4b4bce9b00ebd10c4f3d54ae3e8661e
djeanlouis <danielo.jean-louis@digitaslbi.fr>
2016-02-23 16:32:09 +0100

    add prevent default on input

--- dev/assets/scripts/search-bar.js
+++ dev/assets/scripts/search-bar.js
@@ -10,13 +10,19 @@ var SearchBar = React.createClass({
       this.refs.filterTextInput.value
     );
   },
+
+  submit: function(e) {
+    e.preventDefault();
+  },
+
   render: function() { 
     return (
       <form>
         <input type="text" placeholder="Search..." 
                className="pkmn-searchbar" value={ this.props.filterText }
                defaultValue="Pikachuchu"
-               ref="filterTextInput" onChange={this.handleChange}/>
+               ref="filterTextInput" 
+               onChange={this.handleChange} onSubmit={this.submit}/>
       </form>
     );
   } 
